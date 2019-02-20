3c262837c82d183cced1cbfa1bf74f144bb6ad92
rangarajans <rangarajans.91@gmail.com>
2019-02-20 23:29:36 +1300

    Index and changes
    Index column and automation

--- CurrencyScraper.py
+++ CurrencyScraper.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 import urllib2
 import csv
 import datetime
+import os
 
-
-
+#Base URL
 url = 'https://www.google.com/search?q=nzd+to+inr' 
+
+#Mask bot as a browser
 opener = urllib2.build_opener()
 opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36')]
 openURL = opener.open(url)
@@ -14,44 +16,59 @@ openURL = opener.open(url)
 
 src = BeautifulSoup(openURL.read(), features = "html5lib")
 rate = src.find("span", {"id": "knowledge-currency__tgt-amount"}).get("data-value")
-"""
-date =  datetime.date.today()
-day = datetime.datetime.now().strftime("%A")
-hour = datetime.datetime.now().strftime("%H")
-NZD = 1
-"""
-record = {'row_id':1,
-        'date':datetime.date.today(), 
+
+fieldNames = ['row_id','date','time','day','hour','NZD','INR']
+
+
+# if os.stat('raw_data.csv') == 0:
+#     with open ('raw_data.csv', mode = 'w') as raw_data:
+#         csv.DictWriter(raw_data, fieldnames = fieldNames).writeheader()
+
+with open ('raw_data.csv', mode = 'a') as raw_data:
+    # Fieldnames = ['row_id','date','time','day','hour','NZD','INR']
+    writer = csv.DictWriter(raw_data, fieldnames = fieldNames)
+    writeHeaderFlag = 0
+    while os.stat('raw_data.csv').st_size == 0:
+        writer.writeheader()
+        writeHeaderFlag = 1
+        break
+
+    # Check if the header was written in this iteration, update row index.
+    if writeHeaderFlag == 1:
+        row_id = 1
+        indexFile = open('index.txt', 'w')
+        indexFile.write(str(row_id))
+        indexFile.close()
+    else:
+        indexFile = open('index.txt', 'r')
+        row_id = int(indexFile.read()) + 1
+        indexFile.close()
+        indexFile = open('index.txt', 'w')
+        indexFile.write(str(row_id))
+        indexFile.close()
+
+    record = {'row_id':row_id,
+        'date':datetime.date.today(),
+        'time': datetime.datetime.now().time(),
         'day':datetime.datetime.now().strftime("%A"), 
         'hour':datetime.datetime.now().strftime("%H"), 
         'NZD':1, 
         'INR':rate}
-with open ('raw_data.csv', mode = 'a') as raw_data:
-    Fieldnames = ['row_id','date','day','hour','NZD','INR']
-    writer = csv.DictWriter(raw_data, fieldnames = Fieldnames)
-    #write the header
-    writer.writeheader()
     writer.writerow({'row_id':record['row_id'],
-                    'date':record['date'],
-                    'day':record['day'],
-                    'hour':record['hour'],
-                    'NZD':record['NZD'],
-                    'INR':record['INR']})
-
-"""
-while urllib2.urlopen(url).getcode() == 200:
-    src = BeautifulSoup(html)
-    print src
-
-    title = src.find("div", {"id": "ctitle"}).text
-    div = src.find("div", {"id": "comic"})
-    img = 'http:'+div.find("img")["src"]
-    print img
-    loc = 'F:\\Python\\xkcd\\' + title + ".png"
-    #~ urllib.urlretrieve(img, loc)
-    print src.find("a", {"rel": "next"}).get('href')
- """
-
+                'date':record['date'],
+                'time':record['time'],
+                'day':record['day'],
+                'hour':record['hour'],
+                'NZD':record['NZD'],
+                'INR':record['INR']})
+    #write the header
+    # writer.writeheader()
+    # writer.writerow({'row_id':record['row_id'],
+    #                 'date':record['date'],
+    #                 'day':record['day'],
+    #                 'hour':record['hour'],
+    #                 'NZD':record['NZD'],
+    #                 'INR':record['INR']})
 
 
 """
